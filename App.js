11b620e63541001a63cf70894ab0d76a0709693a
Karolina Kowalczyk <k.kowalczyk@brainforge.pl>
2021-10-18 22:43:32 +0200

    color changes

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0ff',
+    backgroundColor: '#FF0',
     alignItems: 'center',
     justifyContent: 'center',
   },
