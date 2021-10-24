1e02b3e46b1ab924d78a36c33d29f1e8de8b654a
Karolina Kowalczyk <k.kowalczyk@brainforge.pl>
2021-10-24 22:57:04 +0200

    main page added

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -7,10 +7,17 @@ const MainPage = () => {
 
   return (
       <View style={styles.container}>
-          <Avatar.Image size={300} source={require('../assets/images/reading.png')} style={styles.image} />
-          <Text style={styles.infoMain}>Book app</Text>
-          <Link to="/login">
-         <Text>dksak</Text>
+        <Avatar.Image size={300} source={require('../assets/images/reading.png')} style={styles.image} />
+        <Text style={styles.infoMain}>Book app</Text>
+        <Link to="/login">
+          <Button mode="contained" style={styles.subBtn}>
+            <Text style={styles.subBtnText}>ZALOGUJ SIĘ</Text>
+          </Button>
+        </Link>
+        <Link to="/register">
+          <Button mode="contained" style={styles.subBtn}>
+            <Text style={styles.subBtnText}>ZAREJESTRUJ SIĘ</Text>
+          </Button>
         </Link>
     </View>
   );
@@ -18,22 +25,29 @@ const MainPage = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    marginTop: 20,
-    backgroundColor: '#d7e0ff'
-    //justifyContent: 'center',
+      flex: 1,
+      alignItems: 'center',
+      marginTop: 20,
+      backgroundColor: '#d7e0ff'
+      //justifyContent: 'center',
     },
-    image: {
-        marginTop: 30,
-    },
-    infoMain: {
-        fontSize: 36,
-        fontWeight: 'bold',
-        lineHeight: 46,
-        letterSpacing: 0.2,
-    },
-   
+  image: {
+      marginTop: 50,
+  },
+  infoMain: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    lineHeight: 46,
+    letterSpacing: 0.2,
+    marginTop: 50
+  },
+  subBtn: {
+    marginTop: 50,
+    width: 300,
+  },
+  subBtnText: {
+     fontSize: 24,
+  }
 });
 
   
