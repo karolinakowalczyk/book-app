d58e191fe87d88576b8f4a7b1ea8b454ac361949
Nyatan <nyatan.tia@gmail.com>
2021-11-20 13:09:05 +0100

    Minor improvement, search documentation

--- OpenLibraryAPI.js
+++ OpenLibraryAPI.js
@@ -72,47 +72,47 @@ async function getRandomBooks(num=20, fullData=false, keys=importantKeysEdition)
 }
 
 /*
-    query - ambiguous string to search
-    name - filter book titles
-    author - filter author names
-    page - page number to show
-    limit - entry limit per page
-*/
-// async function search(
-//     {
-//         query='', 
-//         name='', 
-//         author='', 
-//         page=1, 
-//         limit=20, 
-//         fullData=false, 
-//         keys=importantKeys
-//     }={}){
-//     var request = ''
-    
-//     if(query != '') request += `q=${query}&`
-//     if(name != '') request += `name=${name}&`
-//     if(author != '') request += `author=${author}&`
-//     if(page > 1 && Number.isInteger(page)) request += `page=${page}&`
-    
-//     if(request=='') throw new Error('No parameters provided.')
-//     request += `limit=${limit}`
+USAGE
+***
+filter - string / array of strings
+value - string / array of [strings or arrays of strings]
 
-//     console.log(parseQuery('search', request))
-//     let data = await fetch(parseQuery('search', request)).then(response => response.json())
-//     if (!fullData) for (let entry in data.docs) data.docs[entry] = fetchMostImportantData(data.docs[entry], keys)
-//     return data
-// }
+Skomplikowane, ale:
+
+1. filter: string, value: string => pojedynczy filtr szukający wszystkiego co w tym polu ma value; np. 'title', 'lord' szuka wsystkich książek z 'lord' w tytule
+2. filter: string, value: arr[str] => pojedynczy filtr szukający wszystkiego co w tym polu ma value[0] ORAZ value[1] itd.; np. 'title', ['lord of', 'rings'] wyszuka wszystko co w tytule zawiera na dowolnych pozycjach stringi 'lord of' (nierozłączne) oraz 'rings'
+3. filter: arr[str], value: arr[str] => wiele filtrów i odpowiadające im pojedyncze wartości; np. ['title', 'author'], ['lord', 'tolkien'] wyszuka wszystkie tytuły co mają w tytule 'lord', a autor ma w nazwie 'tolkien'
+4. filter: arr[str], value: arr[arr[str]] => jak w 2., tylko wiele filtrów jak w 3.
+
+3 i 4 mogą być wymieszane w value, np. ['tolkien', ['lord of', 'rings']]
+***
+FILTERS
+***
+title
+author
+subject
+place
+person
+language => 3 pierwsze litery angielsiej nazwy języka (spa, eng, pol itd.); WYJĄTKI - japoński = jpn; wiele języków = mul; undetermined = und
+publisher
+***
+*/
 
 async function search(filter, value, limit=20, page=1, fullData=false, keys=importantKeys){
     let request = ''
     if(Array.isArray(filter)) 
         for(let i in filter){
-            if(Array.isArray(value)) request += filter[i] + '=' + value[i] + '&'
+            if(Array.isArray(value)){
+                if(Array.isArray(value[i])) request += filter[i] + '=' + value[i].join('+') + '&'
+                else request += filter[i] + '=' + value[i] + '&'
+            }
             else request += filter[i] + '=' + value + '&'
         }
 
-    else request = filter + '=' + value + '&'
+    else {
+        if(Array.isArray(value)) request = filter + '=' + value.join('+') + '&'
+        else request = filter + '=' + value + '&'
+    }
     request += 'page=' + page + '&limit=' + limit
     request = parseQuery('search', request)
     console.log(request)
@@ -137,7 +137,7 @@ async function getImageUrl(id, size='M'){
 //await search({query: 'Tolkien', limit: 1, page: 10})
 //console.log(await getRandomBooks(2))
 //console.log(await getImageUrl('OL7353617M'))
-console.log((await search(['title', 'author'], ['Lord', 'tolkien'])))
+console.log((await search(['title', 'author'], ['Lord of', ['J', 'tolkien']])))
 
 /*
 TODO
